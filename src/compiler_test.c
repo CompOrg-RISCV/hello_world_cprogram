92e71a3ff74f0f0e11f007a83dd4412781c082d1
CompOrg-RISCV <114032938+CompOrg-RISCV@users.noreply.github.com>
2022-12-15 15:52:41 -0800

    Update compiler_test.c

--- src/compiler_test.c
+++ src/compiler_test.c
@@ -5,8 +5,8 @@
 int main() {
     uint32_t number = 20;
 
-    // printf("Input the number of Hello Worlds to print: ");
-    // scanf("%d", &number);
+    printf("Input the number of Hello Worlds to print: ");
+    scanf("%d", &number);
 
     for (uint32_t i = 0; i < number; i++) {
 
